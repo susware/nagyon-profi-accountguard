76a990011e7af0848cb492d4be84ef029ca532ec
Victor <57812807+susware@users.noreply.github.com>
2019-11-26 22:03:55 +0100

    Update Core.java

--- src/ez/susware/ipguard/Core.java
+++ src/ez/susware/ipguard/Core.java
@@ -36,60 +36,60 @@ public class Core extends JavaPlugin implements CommandExecutor, Listener {
 				String path = "ip." + p.getName().toLowerCase();
 				if(args.length == 0) {
 					if(config.contains(path)) {
-						p.sendMessage("§cYou've already protected your ip");
+						p.sendMessage("Â§cYou've already protected your ip");
 						return true;
 					} else {
 						String ip = p.getAddress().toString().replace("/", "");
 						config.set(path, ip);
-						p.sendMessage("§aSuccessfully protected your ip address");
+						p.sendMessage("Â§aSuccessfully protected your ip address");
 					}
 				} else if(args.length == 1) {
 					if(p.isOp()) {
 						Player t = Bukkit.getPlayer(args[0]);
 						if(t == null) {
-							p.sendMessage("§cThis player is currently offline");
+							p.sendMessage("Â§cThis player is currently offline");
 							return true;
 						} else {
 							String pathx = "ip." + t.getName().toLowerCase();
 							String ipx = t.getAddress().toString().replace("/", "");
 							if(config.contains(pathx)) {
-								p.sendMessage("§cThis player already protected his ip");
+								p.sendMessage("Â§cThis player already protected his ip");
 								return true;
 							} else {
 								config.set(pathx, ipx);
-								p.sendMessage("§aSuccessfully protected §7" + t.getName() + "§a's address");
+								p.sendMessage("Â§aSuccessfully protected Â§7" + t.getName() + "Â§a's address");
 							}
 						}
 					} else {
-						p.sendMessage("§cUsage: /ip");
+						p.sendMessage("Â§cUsage: /ip");
 						return true;
 					}
 				} else {
 					if(p.isOp()) {
-						p.sendMessage("§cUsage: /ip (player)");
+						p.sendMessage("Â§cUsage: /ip (player)");
 						return true;
 					} else {
-						p.sendMessage("§cUsage: /ip");
+						p.sendMessage("Â§cUsage: /ip");
 						return true;
 					}
 				}
 			} else {
 				if(args.length != 1) {
-					sender.sendMessage("§cUsage: /ip (player)");
+					sender.sendMessage("Â§cUsage: /ip (player)");
 				} else {
 					Player t = Bukkit.getPlayer(args[0]);
 					if(t == null) {
-						sender.sendMessage("§cThis player is currently offline");
+						sender.sendMessage("Â§cThis player is currently offline");
 						return true;
 					} else {
 						String pathx = "ip." + t.getName().toLowerCase();
 						String ipx = t.getAddress().toString().replace("/", "");
 						if(config.contains(pathx)) {
-							sender.sendMessage("§cThis player already protected his ip");
+							sender.sendMessage("Â§cThis player already protected his ip");
 							return true;
 						} else {
 							config.set(pathx, ipx);
-							sender.sendMessage("§aSuccessfully protected §7" + t.getName() + "§a's address");
+							sender.sendMessage("Â§aSuccessfully protected Â§7" + t.getName() + "Â§a's address");
 						}
 					}
 				}
@@ -102,9 +102,9 @@ public class Core extends JavaPlugin implements CommandExecutor, Listener {
 				if(args.length == 0) {
 					if(config.contains(path)) {
 						config.set(path, null);
-						p.sendMessage("§aSuccessfully removed your ip address");
+						p.sendMessage("Â§aSuccessfully removed your ip address");
 					} else {
-						p.sendMessage("§cYou did not protect his ip yet");
+						p.sendMessage("Â§cYou did not protect his ip yet");
 						return true;
 					}
 				} else if(args.length == 1) {
@@ -112,34 +112,34 @@ public class Core extends JavaPlugin implements CommandExecutor, Listener {
 						String pathx = "ip." + args[0].toLowerCase();
 						if(config.contains(pathx)) {
 							config.set(pathx, null);
-							sender.sendMessage("§aSuccessfully removed §7" + args[0] + "§a's address");
+							sender.sendMessage("Â§aSuccessfully removed Â§7" + args[0] + "Â§a's address");
 						} else {
-							sender.sendMessage("§cThis player did not protect his ip yet");
+							sender.sendMessage("Â§cThis player did not protect his ip yet");
 							return true;
 						}
 					} else {
-						p.sendMessage("§cUsage: /removeip");
+						p.sendMessage("Â§cUsage: /removeip");
 						return true;
 					}
 				} else {
 					if(p.isOp()) {
-						p.sendMessage("§cUsage: /removeip (player)");
+						p.sendMessage("Â§cUsage: /removeip (player)");
 						return true;
 					} else {
-						p.sendMessage("§cUsage: /removeip");
+						p.sendMessage("Â§cUsage: /removeip");
 						return true;
 					}
 				}
 			} else {
 				if(args.length != 1) {
-					sender.sendMessage("§cUsage: /removeip (player)");
+					sender.sendMessage("Â§cUsage: /removeip (player)");
 				} else {
 					String pathx = "ip." + args[0].toLowerCase();
 					if(config.contains(pathx)) {
 						config.set(pathx, null);
-						sender.sendMessage("§aSuccessfully removed §7" + args[0] + "§a's address");
+						sender.sendMessage("Â§aSuccessfully removed Â§7" + args[0] + "Â§a's address");
 					} else {
-						sender.sendMessage("§cThis player did not protect his ip yet");
+						sender.sendMessage("Â§cThis player did not protect his ip yet");
 						return true;
 					}
 				}
@@ -158,7 +158,7 @@ public class Core extends JavaPlugin implements CommandExecutor, Listener {
 			if(p.getAddress().equals(ip.toString())) {
 				return;
 			} else {
-				p.kickPlayer("§cYour ip does not equals to the protected one. Contact an admin");
+				p.kickPlayer("Â§cYour current ip doesn't match with the protected one. Contact an admin");
 				return;
 			}
 		}
